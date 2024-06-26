48c3c2e6e02d08cf5792424e7d7f287b994e259c
robertokbr <robertojuniordev@gmail.com>
2024-06-26 17:53:23 -0300

    updates

--- src/components/organisms/social/social.ts
+++ src/components/organisms/social/social.ts
@@ -2,22 +2,29 @@ import { FlexProps } from "@chakra-ui/react";
 import { IconType } from "react-icons";
 
 export enum SocialIcon {
-  YouTube = 'FaYoutube',
-  Instagram = 'AiFillInstagram',
-  TikTok = 'BsTiktok',
-  Discord = 'BsDiscord',
-  File = 'FaFile',
+  YouTube = 'youtube',
+  Instagram = 'instagram',
+  TikTok = 'tiktok',
+  Discord = 'discord',
+  File = 'file',
 }
+
+export class SocialData {
+  link: string;
+  fallbackLink?: string;
+  title: string;
+  content: string;
+  id: number;
+  icon: SocialIcon;
+
+  constructor(data: Partial<SocialData>) {
+    Object.assign(this, data);
+  }
+};
 
 export interface SocialProps {
   containerProps?: FlexProps;
   icon: SocialIcon;
   isDraggable?: boolean;
-  data: {
-    link: string;
-    fallbackLink?: string;
-    title: string;
-    content: string;
-    id: number;
-  };
+  data: SocialData;
 }
