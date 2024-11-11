dff03776c59c5c37a04aac7870a2e085e099bf7b
robertokbr <robertojuniordev@gmail.com>
2024-11-11 15:19:05 -0300

    feat: add pixel

--- src/components/atoms/feed-head/index.tsx
+++ src/components/atoms/feed-head/index.tsx
@@ -20,6 +20,34 @@ export function FeedHead() {
       <meta property="twitter:title" content={metaConfig.feed.title}/>
       <meta property="twitter:description" content={metaConfig.feed.content}/>
       <meta property="twitter:image" content={metaConfig.feed.image}/>
+
+
+      {/* Meta Pixel Code */}
+      <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '948874153956803');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=948874153956803&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
     </Head>
   );
 }
